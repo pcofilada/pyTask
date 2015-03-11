6e32c71266f9fe1eb68265d932c92fe394d0dbda
Patrick <pcofilada@gmail.com>
2015-03-12 00:18:33 +0800

    Add task on installed apps

--- pyTask/settings.py
+++ pyTask/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication'
+    'authentication',
+    'task'
 )
 
 MIDDLEWARE_CLASSES = (
