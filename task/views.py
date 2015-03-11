bfb57fb77c0e3f01be946d6deef93409c61fc0ba
Patrick <pcofilada@gmail.com>
2015-03-12 01:33:05 +0800

    Add task index view

--- task/views.py
+++ task/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from task.models import Task
 
 # Create your views here.
+class IndexView(generic.ListView):
+	template_name = 'task/index.html'
+	context_object_name = 'latest_task_list'
+
+	def get_queryset(self):
+		return Task.objects.all()
