6e77774e374fd4281620713b9b2ecef4f55e40c4
Patrick <pcofilada@gmail.com>
2015-03-12 00:46:35 +0800

    Add task model migration

--- task/models.py
+++ task/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+	title = models.CharField(max_length=200)
+	start = models.DateTimeField()
+	end = models.DateTimeField()
+	total = models.CharField(max_length=50)
+	user = models.ForeignKey(User)
+
+	def __str__(self):
+		return self.title
